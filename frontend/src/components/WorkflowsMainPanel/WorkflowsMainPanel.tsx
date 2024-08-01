7ab5a667ca3d04fbc08ab5bea47e11a024a46fb2
MRadzynski <radzynski.milosz@gmail.com>
2024-08-01 22:31:19 +0200

    FE: Workflow main panel components

--- frontend/src/components/WorkflowsMainPanel/WorkflowsMainPanel.tsx
+++ frontend/src/components/WorkflowsMainPanel/WorkflowsMainPanel.tsx
@@ -11,6 +11,40 @@ const WorkflowsMainPanel = () => {
           <span className="workflows-breadcrumbs">
             Workflows/{activeWorkflow.name.replace(/ /g, '-')}
           </span>
+          <section className="workflows-workflow">
+            <div className="workflow-details">
+              <div className="workflow-details-group title">
+                <h3 className="workflow-details-group-header">Title</h3>
+                <input
+                  className="workflow-input"
+                  type="text"
+                  value={activeWorkflow.name}
+                />
+              </div>
+              <div className="workflow-details-group function-name">
+                <h3 className="workflow-details-group-header">Function</h3>
+                <input
+                  className="workflow-input"
+                  readOnly
+                  type="text"
+                  value={activeWorkflow.function}
+                />
+              </div>
+              <div className="workflow-details-group input">
+                <h3 className="workflow-details-group-header">Input</h3>
+                <div className="workflow-input-container"></div>
+              </div>
+              <div className="workflow-details-group output">
+                <h3 className="workflow-details-group-header">Output</h3>
+                <div className="workflow-output-container"></div>
+              </div>
+              <div className="workflow-details-group condition">
+                <h3 className="workflow-details-group-header">Condition</h3>
+                <div className="workflow-condition-container"></div>
+              </div>
+            </div>
+            <div className="workflow-steps-container"></div>
+          </section>
         </>
       ) : (
         <p className="workflows-main-panel--empty">
--- frontend/src/data/dummyWorkflows.ts
+++ frontend/src/data/dummyWorkflows.ts
@@ -1,5 +1,6 @@
 export interface Workflow {
   description: string;
+  function: string;
   id: string;
   name: string;
 }
@@ -7,36 +8,43 @@ export interface Workflow {
 export const DUMMY_WORKFLOWS: Workflow[] = [
   {
     description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit',
+    function: 'Function 1',
     id: '1',
     name: 'Workflow 1'
   },
   {
     description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit',
+    function: 'Function 2',
     id: '2',
     name: 'Workflow 2'
   },
   {
     description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit',
+    function: 'Function 3',
     id: '3',
     name: 'Workflow 3'
   },
   {
     description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit',
+    function: 'Function 4',
     id: '4',
     name: 'Workflow 4'
   },
   {
     description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit',
+    function: 'Function 5',
     id: '5',
     name: 'Workflow 5'
   },
   {
     description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit',
+    function: 'Function 6',
     id: '6',
     name: 'Workflow 6'
   },
   {
     description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit',
+    function: 'Function 7',
     id: '7',
     name: 'Workflow 7'
   }
