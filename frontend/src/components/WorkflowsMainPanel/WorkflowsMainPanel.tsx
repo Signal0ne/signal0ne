ff303a16227a786efce527decfd856e122cd0237
MRadzynski <radzynski.milosz@gmail.com>
2024-08-04 09:49:33 +0200

    Workflows: add main panel structure

--- frontend/src/components/WorkflowsMainPanel/WorkflowsMainPanel.tsx
+++ frontend/src/components/WorkflowsMainPanel/WorkflowsMainPanel.tsx
@@ -1,9 +1,47 @@
+import {
+  ArrowDown,
+  BackStageIcon,
+  JaegerIcon,
+  PrometheusIcon,
+  SlackIcon
+} from '../Icons/Icons';
+import { ReactNode, useEffect } from 'react';
 import { useWorkflowsContext } from '../../hooks/useWorkflowsContext';
 import './WorkflowsMainPanel.scss';
 
-const WorkflowsMainPanel = () => {
-  const { activeWorkflow } = useWorkflowsContext();
+const getIntegrationIcon = (integrationName: string) => {
+  const icons: Record<string, ReactNode> = {
+    backstage: <BackStageIcon />,
+    jaeger: <JaegerIcon />,
+    prometheus: <PrometheusIcon />,
+    slack: <SlackIcon />
+  };
 
+  return icons[integrationName] || null;
+};
+
+const WorkflowsMainPanel = () => {
+  const { activeWorkflow, activeStep, setActiveStep } = useWorkflowsContext();
+
+  useEffect(() => {
+    if (!activeWorkflow?.steps[1]) return;
+    setActiveStep(activeWorkflow?.steps[1]);
+  }, [activeWorkflow, setActiveStep]);
+
+  const calcStepsListHeight = () => {
+    const workflowsContainer =
+      document.querySelector('.workflows-workflow')?.getBoundingClientRect()
+        .height ?? 0;
+    const workflowInfoContainer =
+      document
+        .querySelector('.workflow-info-container')
+        ?.getBoundingClientRect().height ?? 0;
+    if (!workflowsContainer) return '100%';
+
+    return workflowsContainer - workflowInfoContainer;
+  };
+
+  console.log(activeWorkflow, activeStep);
   return (
     <main className="workflows-main-panel">
       {activeWorkflow ? (
@@ -14,36 +52,126 @@ const WorkflowsMainPanel = () => {
           <section className="workflows-workflow">
             <div className="workflow-details">
               <div className="workflow-details-group title">
-                <h3 className="workflow-details-group-header">Title</h3>
+                <h3 className="workflow-details-group-header">
+                  Title
+                  <div className="workflow-details-group-header-icon">
+                    {getIntegrationIcon(activeStep?.integration || '')}
+                  </div>
+                </h3>
                 <input
                   className="workflow-input"
+                  readOnly
                   type="text"
-                  value={activeWorkflow.name}
+                  value={activeStep?.name || ''}
                 />
               </div>
-              <div className="workflow-details-group function-name">
+              <div className="workflow-details-group function">
                 <h3 className="workflow-details-group-header">Function</h3>
                 <input
                   className="workflow-input"
                   readOnly
                   type="text"
-                  value={activeWorkflow.function}
+                  value={activeStep?.function || ''}
                 />
               </div>
               <div className="workflow-details-group input">
                 <h3 className="workflow-details-group-header">Input</h3>
-                <div className="workflow-input-container"></div>
+                <div className="workflow-details-group-content input">
+                  {activeStep?.input &&
+                    Object.entries(activeStep.input).map((step, index) => (
+                      <div key={index} className="workflow-group-item">
+                        <span className="workflow-group-item-key">
+                          {`${step[0]}:{{`}
+                        </span>
+                        <span className="workflow-group-item-value">
+                          {step[1]}
+                        </span>
+                        {'}}'}
+                      </div>
+                    ))}
+                </div>
               </div>
               <div className="workflow-details-group output">
                 <h3 className="workflow-details-group-header">Output</h3>
-                <div className="workflow-output-container"></div>
+                <div className="workflow-details-group-content output">
+                  {activeStep?.output &&
+                    Object.entries(activeStep.output).map((step, index) => (
+                      <div key={index} className="workflow-group-item">
+                        <span className="workflow-group-item-key">
+                          {`${step[0]}:{{`}
+                        </span>
+                        <span className="workflow-group-item-value">
+                          {step[1]}
+                        </span>
+                        {'}}'}
+                      </div>
+                    ))}
+                </div>
               </div>
               <div className="workflow-details-group condition">
                 <h3 className="workflow-details-group-header">Condition</h3>
-                <div className="workflow-condition-container"></div>
+                <div className="workflow-details-group-content condition">
+                  {activeStep?.condition && (
+                    <div className="workflow-group-item">
+                      {activeStep.condition}
+                    </div>
+                  )}
+                </div>
               </div>
             </div>
-            <div className="workflow-steps-container"></div>
+            <div className="workflow-steps-container">
+              <div className="workflow-info-container">
+                <h3 className="workflow-info-name">{activeWorkflow.name}</h3>
+                <h5 className="workflow-info-description">
+                  {activeWorkflow.description}
+                </h5>
+              </div>
+              <div
+                className="workflow-steps-list"
+                style={{ height: calcStepsListHeight() }}
+              >
+                {activeWorkflow.steps.map((step, index) => (
+                  <>
+                    <div
+                      className={`workflow-step ${
+                        step.name === activeStep?.name ? 'active' : ''
+                      }`}
+                      key={index}
+                      onClick={() => setActiveStep(step)}
+                    >
+                      <div className="workflow-step-icon">
+                        {getIntegrationIcon(step.integration)}
+                      </div>
+                      <div className="workflow-step-info-container">
+                        <span className="workflow-step-info-name">
+                          {step.name}
+                        </span>
+                        <span className="workflow-step-info-function">
+                          Function: {step.function}
+                        </span>
+                      </div>
+                      <div className="workflow-step-output">
+                        {step?.output &&
+                          Object.entries(step.output).map((output, index) => {
+                            return (
+                              <span
+                                key={index}
+                              >{`${output[0]}:{{${output[1]}}}`}</span>
+                            );
+                          })}
+                      </div>
+                    </div>
+                    {index !== activeWorkflow.steps.length - 1 && (
+                      <ArrowDown
+                        className="workflow-step-separator"
+                        height={36}
+                        width={36}
+                      />
+                    )}
+                  </>
+                ))}
+              </div>
+            </div>
           </section>
         </>
       ) : (
--- frontend/src/data/dummyWorkflows.ts
+++ frontend/src/data/dummyWorkflows.ts
@@ -1,51 +1,333 @@
+export interface Step {
+  condition?: string;
+  function: string;
+  input: {
+    [key: string]: string;
+  };
+  integration: string;
+  name: string;
+  output?: {
+    [key: string]: string;
+  };
+}
+
 export interface Workflow {
   description: string;
-  function: string;
   id: string;
   name: string;
+  steps: Step[];
 }
 
 export const DUMMY_WORKFLOWS: Workflow[] = [
   {
     description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit',
-    function: 'Function 1',
     id: '1',
-    name: 'Workflow 1'
+    name: 'Workflow 1',
+    steps: [
+      {
+        name: 'Get ownership',
+        integration: 'backstage',
+        function: 'get_properties_values',
+        input: {
+          filter: 'kind=component,metadata.name={service}'
+        },
+        output: {
+          owner: 'spec.owner',
+          slack_channel: 'metadata.labels.slack_channel'
+        }
+      },
+      {
+        name: 'Inspect traces',
+        integration: 'jaeger',
+        function: 'log_occurrences',
+        input: {
+          service: '{service}',
+          tags: "{'error': true}"
+        },
+        output: {
+          count: 'sum()',
+          log: 'logs:exception.stacktrace | tags:grpc.error_message'
+        },
+        condition: 'empty(additional_context.log_occurrences)'
+      },
+      {
+        name: 'Send to slack channel',
+        integration: 'slack',
+        function: 'post_message',
+        input: {
+          parsable_context_object: '.',
+          ignore_context_keys: 'additional_context.components[].slack_channel'
+        },
+        condition: 'greater(additional_context.log_occurrences.count'
+      },
+      {
+        name: 'Send to slack channel',
+        integration: 'slack',
+        function: 'post_message',
+        input: {
+          parsable_context_object: '.',
+          ignore_context_keys: 'additional_context.components[].slack_channel'
+        },
+        condition: 'greater(additional_context.log_occurrences.count'
+      }
+    ]
   },
   {
     description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit',
-    function: 'Function 2',
     id: '2',
-    name: 'Workflow 2'
+    name: 'Workflow 2',
+    steps: [
+      {
+        name: 'Get ownership',
+        integration: 'backstage',
+        function: 'get_properties_values',
+        input: {
+          filter: 'kind=component,metadata.name={service}'
+        },
+        output: {
+          owner: 'spec.owner',
+          slack_channel: 'metadata.labels.slack_channel'
+        }
+      },
+      {
+        name: 'Inspect traces',
+        integration: 'jaeger',
+        function: 'log_occurrences',
+        input: {
+          service: '{service}',
+          tags: "{'error': true}"
+        },
+        output: {
+          count: 'sum()',
+          log: 'logs:exception.stacktrace | tags:grpc.error_message'
+        },
+        condition: 'empty(additional_context.log_occurrences)'
+      },
+      {
+        name: 'Send to slack channel',
+        integration: 'slack',
+        function: 'post_message',
+        input: {
+          parsable_context_object: '.',
+          ignore_context_keys: 'additional_context.components[].slack_channel'
+        },
+        condition: 'greater(additional_context.log_occurrences.count'
+      }
+    ]
   },
   {
     description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit',
-    function: 'Function 3',
     id: '3',
-    name: 'Workflow 3'
+    name: 'Workflow 3',
+    steps: [
+      {
+        name: 'Get ownership',
+        integration: 'backstage',
+        function: 'get_properties_values',
+        input: {
+          filter: 'kind=component,metadata.name={service}'
+        },
+        output: {
+          owner: 'spec.owner',
+          slack_channel: 'metadata.labels.slack_channel'
+        }
+      },
+      {
+        name: 'Inspect traces',
+        integration: 'jaeger',
+        function: 'log_occurrences',
+        input: {
+          service: '{service}',
+          tags: "{'error': true}"
+        },
+        output: {
+          count: 'sum()',
+          log: 'logs:exception.stacktrace | tags:grpc.error_message'
+        },
+        condition: 'empty(additional_context.log_occurrences)'
+      },
+      {
+        name: 'Send to slack channel',
+        integration: 'slack',
+        function: 'post_message',
+        input: {
+          parsable_context_object: '.',
+          ignore_context_keys: 'additional_context.components[].slack_channel'
+        },
+        condition: 'greater(additional_context.log_occurrences.count'
+      }
+    ]
   },
   {
     description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit',
-    function: 'Function 4',
     id: '4',
-    name: 'Workflow 4'
+    name: 'Workflow 4',
+    steps: [
+      {
+        name: 'Get ownership',
+        integration: 'backstage',
+        function: 'get_properties_values',
+        input: {
+          filter: 'kind=component,metadata.name={service}'
+        },
+        output: {
+          owner: 'spec.owner',
+          slack_channel: 'metadata.labels.slack_channel'
+        }
+      },
+      {
+        name: 'Inspect traces',
+        integration: 'jaeger',
+        function: 'log_occurrences',
+        input: {
+          service: '{service}',
+          tags: "{'error': true}"
+        },
+        output: {
+          count: 'sum()',
+          log: 'logs:exception.stacktrace | tags:grpc.error_message'
+        },
+        condition: 'empty(additional_context.log_occurrences)'
+      },
+      {
+        name: 'Send to slack channel',
+        integration: 'slack',
+        function: 'post_message',
+        input: {
+          parsable_context_object: '.',
+          ignore_context_keys: 'additional_context.components[].slack_channel'
+        },
+        condition: 'greater(additional_context.log_occurrences.count'
+      }
+    ]
   },
   {
     description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit',
-    function: 'Function 5',
     id: '5',
-    name: 'Workflow 5'
+    name: 'Workflow 5',
+    steps: [
+      {
+        name: 'Get ownership',
+        integration: 'backstage',
+        function: 'get_properties_values',
+        input: {
+          filter: 'kind=component,metadata.name={service}'
+        },
+        output: {
+          owner: 'spec.owner',
+          slack_channel: 'metadata.labels.slack_channel'
+        }
+      },
+      {
+        name: 'Inspect traces',
+        integration: 'jaeger',
+        function: 'log_occurrences',
+        input: {
+          service: '{service}',
+          tags: "{'error': true}"
+        },
+        output: {
+          count: 'sum()',
+          log: 'logs:exception.stacktrace | tags:grpc.error_message'
+        },
+        condition: 'empty(additional_context.log_occurrences)'
+      },
+      {
+        name: 'Send to slack channel',
+        integration: 'slack',
+        function: 'post_message',
+        input: {
+          parsable_context_object: '.',
+          ignore_context_keys: 'additional_context.components[].slack_channel'
+        },
+        condition: 'greater(additional_context.log_occurrences.count'
+      }
+    ]
   },
   {
     description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit',
-    function: 'Function 6',
     id: '6',
-    name: 'Workflow 6'
+    name: 'Workflow 6',
+    steps: [
+      {
+        name: 'Get ownership',
+        integration: 'backstage',
+        function: 'get_properties_values',
+        input: {
+          filter: 'kind=component,metadata.name={service}'
+        },
+        output: {
+          owner: 'spec.owner',
+          slack_channel: 'metadata.labels.slack_channel'
+        }
+      },
+      {
+        name: 'Inspect traces',
+        integration: 'jaeger',
+        function: 'log_occurrences',
+        input: {
+          service: '{service}',
+          tags: "{'error': true}"
+        },
+        output: {
+          count: 'sum()',
+          log: 'logs:exception.stacktrace | tags:grpc.error_message'
+        },
+        condition: 'empty(additional_context.log_occurrences)'
+      },
+      {
+        name: 'Send to slack channel',
+        integration: 'slack',
+        function: 'post_message',
+        input: {
+          parsable_context_object: '.',
+          ignore_context_keys: 'additional_context.components[].slack_channel'
+        },
+        condition: 'greater(additional_context.log_occurrences.count'
+      }
+    ]
   },
   {
     description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit',
-    function: 'Function 7',
     id: '7',
-    name: 'Workflow 7'
+    name: 'Workflow 7',
+    steps: [
+      {
+        name: 'Get ownership',
+        integration: 'backstage',
+        function: 'get_properties_values',
+        input: {
+          filter: 'kind=component,metadata.name={service}'
+        },
+        output: {
+          owner: 'spec.owner',
+          slack_channel: 'metadata.labels.slack_channel'
+        }
+      },
+      {
+        name: 'Inspect traces',
+        integration: 'jaeger',
+        function: 'log_occurrences',
+        input: {
+          service: '{service}',
+          tags: "{'error': true}"
+        },
+        output: {
+          count: 'sum()',
+          log: 'logs:exception.stacktrace | tags:grpc.error_message'
+        },
+        condition: 'empty(additional_context.log_occurrences)'
+      },
+      {
+        name: 'Send to slack channel',
+        integration: 'slack',
+        function: 'post_message',
+        input: {
+          parsable_context_object: '.',
+          ignore_context_keys: 'additional_context.components[].slack_channel'
+        },
+        condition: 'greater(additional_context.log_occurrences.count'
+      }
+    ]
   }
 ];
