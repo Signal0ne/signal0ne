97eb42f8772a2f46b0a025651564452d0ce88d23
SzymonSt <szymonst2808@gmail.com>
2024-08-21 12:58:00 +0200

    bugfix: fetching alerts alertmanager integratin

--- backend/pkg/integrations/alertmanager/alertmanager_integration.go
+++ backend/pkg/integrations/alertmanager/alertmanager_integration.go
@@ -131,6 +131,8 @@ func getAlerts(url string, filters []string) ([]any, error) {
 		}
 	}
 
+	fmt.Printf("URL %s", url)
+
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, err
