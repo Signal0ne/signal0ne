68946d1caa437fd87ba9546afbbe413dc43fe1c2
SzymonSt <szymonst2808@gmail.com>
2024-08-25 00:19:59 +0200

    requested changes

--- backend/api/routers/main.router.go
+++ backend/api/routers/main.router.go
@@ -32,39 +32,39 @@ func NewMainRouter(
 
 func (r *MainRouter) RegisterRoutes(rg *gin.RouterGroup) {
 
-	namespaceGroup := rg.Group("/namespace")
-	{
-		namespaceGroup.POST("/create")
-		namespaceGroup.GET("/:namespaceid/get")
-		namespaceGroup.DELETE("/:namespaceid/delete")
-		namespaceGroup.PATCH("/:namespaceid/update")
-	}
-
-	workflowGroup := rg.Group("/:namespaceid/workflow")
-	{
-		workflowGroup.POST("/create", r.WorkflowController.ApplyWorkflow)
-		workflowGroup.GET("/:workflowid")
-		workflowGroup.DELETE("/:workflowid")
-		workflowGroup.PATCH("/:workflowid")
-	}
-
 	alertGroup := rg.Group("/alert")
 	{
+		alertGroup.GET("/:alertid/correlations", r.AlertController.Correlations)
 		alertGroup.GET("/:alertid/details", r.AlertController.Details)
 		alertGroup.GET("/:alertid/summary", r.AlertController.Summary)
-		alertGroup.GET("/:alertid/correlations", r.AlertController.Correlations)
 	}
 
 	integrationGroup := rg.Group("/:namespaceid/integration")
 	{
 		integrationGroup.POST("/create", r.IntegrationController.Install)
-		integrationGroup.GET("/:integrationid/get")
 		integrationGroup.DELETE("/:integrationid/delete")
+		integrationGroup.GET("/:integrationid/get")
 		integrationGroup.PATCH("/:integrationid/update")
+	}
+
+	namespaceGroup := rg.Group("/namespace")
+	{
+		namespaceGroup.POST("/create")
+		namespaceGroup.DELETE("/:namespaceid/delete")
+		namespaceGroup.GET("/:namespaceid/get")
+		namespaceGroup.PATCH("/:namespaceid/update")
 	}
 
 	webhookGroup := rg.Group("/webhook")
 	{
 		webhookGroup.POST("/:namespaceid/:workflowid/:salt", r.WorkflowController.WebhookTriggerHandler)
+	}
+
+	workflowGroup := rg.Group("/:namespaceid/workflow")
+	{
+		workflowGroup.POST("/create", r.WorkflowController.ApplyWorkflow)
+		workflowGroup.DELETE("/:workflowid")
+		workflowGroup.GET("/:workflowid")
+		workflowGroup.PATCH("/:workflowid")
 	}
 }
--- backend/pkg/integrations/slack/config.go
+++ backend/pkg/integrations/slack/config.go
@@ -1,7 +1,7 @@
 package slack
 
 type Config struct {
-	WorkspaceID string `json:"workspaceId"`
 	Host        string `json:"host"`
 	Port        string `json:"port"`
+	WorkspaceID string `json:"workspaceId"`
 }
