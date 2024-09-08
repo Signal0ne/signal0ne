8df30081d96a3b7a184ce9bbad422f6b87cfe391
SzymonSt <szymonst2808@gmail.com>
2024-09-08 13:02:08 +0200

    feature: initial confluence logic - quering

--- backend/pkg/integrations/confluence/config.go
+++ backend/pkg/integrations/confluence/config.go
@@ -1,6 +1,7 @@
 package confluence
 
 type Config struct {
+	Url    string `json:"url" bson:"url"`
 	Email  string `json:"email" bson:"email"`
 	APIKey string `json:"apiKey" bson:"apiKey"`
 }
--- backend/pkg/integrations/confluence/confluence_integration.go
+++ backend/pkg/integrations/confluence/confluence_integration.go
@@ -1,7 +1,11 @@
 package confluence
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"signal0ne/internal/models"
 	"signal0ne/internal/tools"
 	"signal0ne/pkg/integrations/helpers"
@@ -77,6 +81,61 @@ func search(input any, integration any) ([]any, error) {
 		return output, err
 	}
 
+	assertedIntegration, _ := integration.(ConfluenceIntegration)
+
+	//Hardcoded limit for UX readability reasons
+	url := fmt.Sprintf("%s/rest/api/content?limit=6&cql=%s", assertedIntegration.Url, parsedInput.Query)
+	basicCredentials := base64.RawStdEncoding.EncodeToString([]byte(assertedIntegration.Email + ":" + assertedIntegration.APIKey))
+
+	contents, err := getPageContent(url, basicCredentials)
+	if err != nil {
+		return output, err
+	}
+
+	for _, content := range contents {
+		output = append(output, content)
+	}
+
 	return output, nil
 
+}
+
+func getPageContent(url string, credentials string) ([]string, error) {
+	var contents []string
+	var results []any
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return contents, err
+	}
+
+	req.Header.Set("Authorization", "Basic "+credentials)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return contents, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return contents, fmt.Errorf("failed to get content from confluence")
+	}
+
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return contents, err
+	}
+
+	err = json.Unmarshal(responseBody, &results)
+	if err != nil {
+		return contents, err
+	}
+
+	for _, result := range results {
+		content := result.(map[string]any)["body"].(map[string]any)["view"].(map[string]any)["value"].(string)
+		contents = append(contents, content)
+	}
+
+	return contents, nil
 }
